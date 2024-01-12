56197a460001995b05df9465f59fa7b69f6c1fef
carlosavellar <carlos.avellar@gmail.com>
2024-01-12 19:41:23 -0300

    v.04.5

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,4 @@
+const corsProxyUrl = "https://cors-anywhere.herokuapp.com/";
 import { AppDispatch } from "@/store";
 import {
   fetchDataStart,
@@ -9,7 +10,14 @@ export const fetchData = () => async (dispatch: AppDispatch) => {
   try {
     dispatch(fetchDataStart());
     const response = await fetch(
-      "https://2486713dae314753ae6b0ff127002d12.api.mockbin.io/"
+      "https://cors-anywhere.herokuapp.com/https://burguer-shop-main-08acd95.d2.zuplo.dev/foods",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Origin: "https://portal.zuplo.com",
+        },
+      }
     );
     const data = await response.json();
     dispatch(fetchDataSuccess(data));
