fadd5ae52f2956ffc41103a2238614504c878202
carlosavellar <carlos.avellar@gmail.com>
2024-01-27 11:16:45 -0300

    v.06.3

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Badge, Col, Container, Row } from "reactstrap";
+import { Badge, Card, Col, Container, Row } from "reactstrap";
 
 import { Accordion, AccordionHeader, AccordionItem } from "reactstrap";
 
@@ -67,69 +67,73 @@ export default function App() {
       <SearchInput />
       <Container>
         <Row>
-          <Col className="custom-box-shadow m-3" md={7} lg={7} xl={7}>
-            <Container>
-              <Row>
-                <Col>
-                  <Row>
-                    <SectionList
-                      menuItems={loadedMenu}
-                      onToggleBurger={toggleBurger}
-                      onToggleDrinks={toggleDrinks}
-                    />
-                  </Row>
-                  <Row className="burger-list">
-                    <Col>
-                      <Accordion
-                        flush
-                        open={sectionToggle}
-                        toggle={toggleBurger}
-                      >
-                        <AccordionItem key={1}>
-                          <AccordionHeader targetId="1">
-                            <h3>Burgers</h3>
-                          </AccordionHeader>
-                          <ProductImageList
-                            sectionId={242403}
-                            menuItems={loadedMenu}
-                            isSectionImage={true}
-                          />
-                        </AccordionItem>
-                      </Accordion>
-                    </Col>
-                  </Row>
-                  <Row className="drink-list">
-                    <Col>
-                      <Accordion
-                        flush
-                        open={sectionToggleDrink}
-                        toggle={toggleDrinks}
-                      >
-                        <AccordionItem key={1}>
-                          <AccordionHeader targetId="1">
-                            <h3>Drinks</h3>
-                          </AccordionHeader>
-                          <ProductImageList
-                            sectionId={242404}
-                            menuItems={loadedMenu}
-                          />
-                        </AccordionItem>
-                      </Accordion>
-                    </Col>
-                  </Row>
-                  <Row className="info">
-                    <Col>
-                      <Link to="/allergy-information" className="nav-link">
-                        <Badge color="white">View allergy information</Badge>
-                      </Link>
-                    </Col>
-                  </Row>
-                </Col>
-              </Row>
-            </Container>
+          <Col className="" md={8} lg={8} xl={8}>
+            <Card className="shadow p-4">
+              <Container>
+                <Row>
+                  <Col>
+                    <Row>
+                      <SectionList
+                        menuItems={loadedMenu}
+                        onToggleBurger={toggleBurger}
+                        onToggleDrinks={toggleDrinks}
+                      />
+                    </Row>
+                    <Row className="burger-list">
+                      <Col>
+                        <Accordion
+                          flush
+                          open={sectionToggle}
+                          toggle={toggleBurger}
+                        >
+                          <AccordionItem key={1}>
+                            <AccordionHeader targetId="1">
+                              <h3>Burgers</h3>
+                            </AccordionHeader>
+                            <ProductImageList
+                              sectionId={242403}
+                              menuItems={loadedMenu}
+                              isSectionImage={true}
+                            />
+                          </AccordionItem>
+                        </Accordion>
+                      </Col>
+                    </Row>
+                    <Row className="drink-list">
+                      <Col>
+                        <Accordion
+                          flush
+                          open={sectionToggleDrink}
+                          toggle={toggleDrinks}
+                        >
+                          <AccordionItem key={1}>
+                            <AccordionHeader targetId="1">
+                              <h3>Drinks</h3>
+                            </AccordionHeader>
+                            <ProductImageList
+                              sectionId={242404}
+                              menuItems={loadedMenu}
+                            />
+                          </AccordionItem>
+                        </Accordion>
+                      </Col>
+                    </Row>
+                    <Row className="info">
+                      <Col>
+                        <Link to="/allergy-information" className="nav-link">
+                          <Badge color="white">View allergy information</Badge>
+                        </Link>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </Container>
+            </Card>
           </Col>
-          <Col className="custom-box-shadow m-3" md={4} lg={4}>
-            <Basket />
+          <Col className="p-10" md={4} lg={4}>
+            <Card className="shadow p-4">
+              <Basket />
+            </Card>
           </Col>
         </Row>
       </Container>
