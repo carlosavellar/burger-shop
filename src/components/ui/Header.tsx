e93b7d95af05d022ecb5e7b5f2aaec35aff57636
jose.santos-k2p <jose.santos-k2p@parceirosedu.com.br>
2025-05-16 15:52:36 -0300

    Fix Header Image

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,18 +1,22 @@
 import "./Header.scss";
-import headerBg from "./../../assets/header.png";
 
+import headerBg from "./../../assets/header.png";
 import React from "react";
 import { Col, Container, Row } from "reactstrap";
 
 function Header() {
   return (
-    <div className='header-index p-0'>
-      <Container fluid style={{ background: `url(${headerBg}) center` }}>
+    <>
+      <Container
+        fluid
+        className='header-index p-0'
+        style={{ background: `url(${headerBg}) center` }}
+      >
         <Row style={{ padding: 0 }}>
           <Col style={{ padding: 0 }}></Col>
         </Row>
       </Container>
-    </div>
+    </>
   );
 }
 
